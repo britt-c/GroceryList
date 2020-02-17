7deaf68d29ae232bd01bd29082dafcb52ddb74b3
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-16 20:35:35 -0800

    adding some styles.. Cant figure out how to save the quantity value along with the text to be displayed together as one item.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-	
+import './App.css';
+
 	const App = ()=> {
 		const [textareaValue, setTextareaValue] = useState("")
 		const [items, setItems] = useState([])
 		const listItems = items.map((item, i)=> (
 			<li key={i}>
 				<span>
-					<input type="number" min="1" max="99" />
-					{item} 
+					<span className="list-item">{item}</span> 
 					<input type="checkbox" />
+					<br />
+					<label htmlFor="quantity">Quantity</label><br />
+					<input placeholder="0" type="number" min="1" max="99" />
 				</span>
 			</li>
 		))
@@ -17,6 +20,7 @@ import React, { useState } from 'react';
 			setTextareaValue(event.currentTarget.value)
 		}
 
+
 		const handleButtonClick = (event)=> {
 			setItems([...items, textareaValue])
 		}
@@ -24,10 +28,12 @@ import React, { useState } from 'react';
 		return (
 			<div className="App">
 				<header className="list-title">Grocery List</header>
-				<textarea onChange={storeText}placeholder="What to get..."></textarea>
+				<br />
+				<input onChange={storeText} placeholder="What to get..." />
 				<br/>
 				<button onClick={handleButtonClick}>Add</button>
 				<ul>{listItems}</ul>
+
 			</div>
 		);
 	}
